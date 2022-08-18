e0b5fbf003c4f6fb43a160cdbdae04ebc4014922
Chris Young <ctopheryoung@gmail.com>
2022-08-18 13:18:10 -0700

    Add fileSytem data object

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
+const fileSystem = {
+  type: "folder",
+  name: "Files",
+  modified: new Date(),
+  size: 1280,
+  children: [
+    {
+      type: "folder",
+      name: "Documents",
+      modified: new Date(),
+      size: 1280,
+      children: []
+    },
+    {
+      type: "folder",
+      name: "Images",
+      modified: new Date(),
+      size: 1280,
+      children: []
+    },
+    {
+      type: "folder",
+      name: "System",
+      modified: new Date(),
+      size: 1280,
+      children: []
+    },
+    {
+      type: "file",
+      name: "Description.rtf",
+      modified: new Date(),
+      size: 1280,
+      children: []
+    },
+    {
+      type: "file",
+      name: "Description.txt",
+      modified: new Date(),
+      size: 1280,
+      children: []
+    },
+  ]
+}
+
 console.log('Howdy again, partner.')

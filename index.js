80a4ad48f06c09f548f70d716a8623bf8cf9ef58
Chris Young <ctopheryoung@gmail.com>
2022-08-18 20:25:35 -0700

    Use JS to create tree folder from data

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const fileSystem = {
+const root = {
   type: "folder",
   name: "Files",
   modified: new Date(),
@@ -10,6 +10,13 @@ const fileSystem = {
       modified: new Date(),
       size: 1280,
       children: [
+        {
+          type: "folder",
+          name: "Images",
+          modified: new Date(),
+          size: 1280,
+          children: [],
+        },
         {
           type: "file",
           name: "Taxes.rtf",
@@ -57,4 +64,38 @@ const fileSystem = {
   ],
 };
 
-console.log("Howdy again, partner.");
+const $folderTree = document.getElementById("folder-tree");
+
+function selectFolder(folder) {
+  console.log(folder);
+}
+
+function createFolderTreeItem(item) {
+  let $el = document.createElement("div");
+
+  $el.className = "folder-tree__folder";
+  $el.innerHTML = `
+    <div class="folder-tree__label">
+      <span>▾</span>
+      <button class="folder-button">
+        <span>📂</span>
+        <span>${item.name}</span>
+      </button>
+    </div>
+  `;
+
+  return $el;
+}
+
+function appendFolderTreeItems(element, folder) {
+  if (folder.type === "folder") {
+    const $el = createFolderTreeItem(folder);
+    $el.querySelector(".folder-button").onclick = () => selectFolder(folder);
+
+    element.append($el);
+
+    folder.children.forEach((child) => appendFolderTreeItems($el, child));
+  }
+}
+
+appendFolderTreeItems($folderTree, root);
